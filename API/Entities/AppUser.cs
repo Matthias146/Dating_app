da45bd936ef2ebc2fe400602a722f9b92579d77c
Matthias Hammelehle <209659387+Matthias146@users.noreply.github.com>
2026-06-16 22:20:18 +0200

    feat(Entities): added AppUser Class and install Entity Framework

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -2,5 +2,10 @@
 
 public class AppUser
 {
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public required string DisplayName { get; set; }
 
+     public required string Email { get; set; }
+    
 }
+
