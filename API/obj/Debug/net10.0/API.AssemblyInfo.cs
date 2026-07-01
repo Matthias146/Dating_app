e04519a1a5f119924be226993516e564cba1d318
Matthias Hammelehle <209659387+Matthias146@users.noreply.github.com>
2026-07-01 21:38:41 +0200

    chore: added secure https to the angular localhos

--- API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9777803b0da5d66bc4966010ae5ebb530f9a62d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5955b116b816ec5f04eee6928e1c6d503bed7632")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
